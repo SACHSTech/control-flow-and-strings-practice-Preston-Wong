9d1e49b6a63c1a18c2dd0bf709b42a34857d5936
Preston-Wong <you@example.com>
2023-10-19 09:40:56 -0400

    A1FreezingHot finished

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -1,1 +1,27 @@
+class A1FreezingHot extends ConsoleProgram {
 
+  /**
+  * Description
+  * @author: Preston Wong
+  */
+  
+  public void run() {
+    
+    Integer intTemp;
+    
+    // Getting user input for the temperature for their water 
+    intTemp = readInt("What is your water temperature? ");
+
+    // Sees is the water is forzen, warm, or boiling 
+    if (intTemp < 0) {
+        System.out.println("Your water is frozen and is now ice");
+    }
+    else if (intTemp > 0 && intTemp < 100) {
+        System.out.println("Your water is warm but not hot");
+    }
+    else {
+        System.out.println("Your water is boiling hot");
+    }
+    
+  }
+}
