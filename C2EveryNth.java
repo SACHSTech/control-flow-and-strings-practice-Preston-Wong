6c7d100c3cc6a280bbcc26314b62822a277bdb57
Preston-Wong <you@example.com>
2023-10-20 15:58:19 -0400

    Every ninth completed

--- C2EveryNth.java
+++ C2EveryNth.java
@@ -1,1 +1,19 @@
 
+class C2EveryNth extends ConsoleProgram {
+
+  /**
+  * Description: Prints out every ninth number 
+  * @author: Preston Wong
+  */
+  
+  public void run() {
+    
+    int counter =0;
+
+    while (counter <= 10) {
+        System.out.println(counter*9);
+        counter++;
+    }
+    
+  }
+}
