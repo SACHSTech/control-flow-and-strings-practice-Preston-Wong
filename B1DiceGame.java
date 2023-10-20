b3f37914f436e2c241edcac8e96f0e55a7f355f3
Preston-Wong <you@example.com>
2023-10-20 16:29:15 -0400

    Commented on B1

--- B1DiceGame.java
+++ B1DiceGame.java
@@ -9,19 +9,25 @@ class B1DiceGame extends ConsoleProgram {
   
   public void run() {
     
+    // randomizer 
     Random myRandom = new Random();
     
+    // variables 
     int intRandom = myRandom.nextInt((6-1)+1)+1;
     int intGuess;
 
+    // gets the user's first guess 
     intGuess = readInt("Guess which side the dice landed on (1-6): ");
 
+    // determines if the number inputted is too high, low, or is correct 
     while (intGuess > intRandom) {
+        // give the user a hint that their number is too high 
         System.out.println("That is too high, guess lower");
         intGuess = readInt("Guess which side the dice landed on (1-6): ");
     }
     
     while (intGuess < intRandom) {
+        // gives the user a hint taht their number is too low 
         System.out.println("That is too low, guess higher ");
         intGuess = readInt("Guess which side the dice landed on (1-6): ");
     }
