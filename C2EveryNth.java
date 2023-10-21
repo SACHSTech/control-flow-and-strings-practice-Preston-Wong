fdffcf152a38ca3f26a64d073139f9c7ca26e159
Preston-Wong <you@example.com>
2023-10-21 16:04:42 -0400

    Completed C2EveryNth

--- C2EveryNth.java
+++ C2EveryNth.java
@@ -2,18 +2,36 @@
 class C2EveryNth extends ConsoleProgram {
 
   /**
-  * Description: Prints out every ninth number 
+  * Description: finds and removes letters in a sequence of letters in terms of i
   * @author: Preston Wong
   */
   
   public void run() {
-    
-    int counter =0;
 
-    while (counter <= 10) {
-        System.out.println(counter*9);
-        counter++;
-    }
+    // variables
+    String strWord;
+    Integer intRemoved;
+    Integer intLength;
+    String strLetter;
+    String strNewLetter;
+
+    // getting user input 
+    strWord = readLine("is your sequence of letters? ");
+    intRemoved = readInt("What factor did you want your letters to be removed at? ");
+
+    // calcualting the starting length of the sequence of letters 
+    intLength = strWord.length();
     
+    // finds which letters to remove in terms of i 
+    for (int i = intRemoved; i < intLength; i++){
+        strLetter = strWord.substring(i - intRemoved + 1 , i);
+        intLength = strWord.length();
+        strNewLetter = strWord.replace(strLetter,"" );
+        strWord = strNewLetter; 
+    }
+
+    // displays the final result 
+    System.out.println(strWord);
+
   }
 }
