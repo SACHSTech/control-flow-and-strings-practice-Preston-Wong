e67f72db5f0282f96fb51bcd948a2418aef536a7
Preston-Wong <you@example.com>
2023-10-21 13:46:18 -0400

    Finisehd C1BackToBack

--- C1BackToBack.java
+++ C1BackToBack.java
@@ -1,1 +1,27 @@
 
+class C1BackToBack extends ConsoleProgram {
+
+  /**
+  * Description: take the last letter of the inputted word and places at the start and end of the word 
+  * @author: Preston Wong
+  */
+  
+  public void run() {
+    
+    // variabels 
+    String strWord;
+    Integer intLength;
+    String strLetter; 
+
+    // get user input 
+    strWord = readLine("What is your word? ");
+
+    // determines what the last letter is 
+    intLength = strWord.length();
+    strLetter = strWord.substring(intLength - 1);
+
+    // outputs final results 
+    System.out.println(strWord + " becomes ");
+    System.out.println(strLetter + strWord + strLetter);
+  }
+}
